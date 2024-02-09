15942ce014b3c3e7ef5597f504985381972b9137
Zubair Alam <syedzubairalam123@gmail.com>
2024-02-09 19:40:46 +0500

    Test commit

--- src/test/java/MobileRunner.java
+++ src/test/java/MobileRunner.java
@@ -1,11 +1,14 @@
-import io.cucumber.testng.CucumberOptions;
+import io.cucumber.junit.Cucumber;
+import io.cucumber.junit.CucumberOptions;
 import org.junit.runner.RunWith;
-@RunWith(io.cucumber.junit.Cucumber.class)
-@CucumberOptions(features = {"src/test/resources/features.app.android/HomePage.feature"},
-        glue = {"src/test/java/step_defination"},
+
+@RunWith(Cucumber.class)
+@CucumberOptions(
+        features = {"src/test/resources/features/app/android/HomePage.feature"},
+        glue = {"step_defination"},
         plugin = { "pretty", "html:target/cucumber-reports" },
         monochrome = true,
-        tags = "@homePage")
-
+        tags = "@homePage"
+)
 public class MobileRunner {
 }
