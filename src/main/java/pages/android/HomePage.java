833a7d9d22ab46c848ba287afdc568ad7c7b9047
Zubair Alam <syedzubairalam123@gmail.com>
2024-02-16 19:47:43 +0500

    new methods

--- src/main/java/pages/android/HomePage.java
+++ src/main/java/pages/android/HomePage.java
@@ -1,6 +1,7 @@
 package pages.android;
 
 import core.utils.AndroidCore.AndroidDriverSetup;
+import io.appium.java_client.MobileBy;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebElement;
 import pages.Page;
@@ -10,9 +11,13 @@ public class HomePage {
     private String titleXpath = "//android.view.ViewGroup/android.widget.TextView";
     private String immediateButtonID = "com.meritnation.store.testingapp:id/btn1";
     private String flexibleButtonID = "com.meritnation.store.testingapp:id/btn2";
-    private String versionCodeID ="com.meritnation.store.testingapp:id/ver_code";
-    private String versionNameID ="com.meritnation.store.testingapp:id/ver_name";
-    private String resultText ="com.meritnation.store.testingapp:id/txt";
+    private String versionCodeID = "com.meritnation.store.testingapp:id/ver_code";
+    private String versionNameID = "com.meritnation.store.testingapp:id/ver_name";
+    private String resultText = "com.meritnation.store.testingapp:id/txt";
+    private String getTitle = "//android.widget.TextView[@text='The App']";
+    private String TextFieldDemoApp = "messageInput";
+    private String getSaveButton = "//android.view.ViewGroup[@content-desc='messageSaveBtn']/android.widget.TextView";
+
 
     public HomePage(Page page) {
     }
@@ -30,7 +35,7 @@ public class HomePage {
     }
 
     public WebElement getButton(int num) {
-        return (AndroidDriverSetup.getAndroidDriver().findElement(By.id("com.meritnation.store.testingapp:id/btn"+num)));
+        return (AndroidDriverSetup.getAndroidDriver().findElement(By.id("com.meritnation.store.testingapp:id/btn" + num)));
     }
 
     public WebElement getImmediateButton() {
@@ -46,6 +51,35 @@ public class HomePage {
     }
 
     public void backButton() {
-         AndroidDriverSetup.getAndroidDriver().navigate().back();
+        AndroidDriverSetup.getAndroidDriver().navigate().back();
+    }
+
+    public WebElement getTitleDemoApp() {
+        return (AndroidDriverSetup.getAndroidDriver().findElement(By.xpath(getTitle)));
+    }
+
+    public WebElement getTabNameDemoApp(String name) {
+        return (AndroidDriverSetup.getAndroidDriver().findElementByAccessibilityId(name));
+    }
+
+    public WebElement getTextFieldDemoApp() {
+        return (AndroidDriverSetup.getAndroidDriver().findElementByAccessibilityId(TextFieldDemoApp));
+    }
+
+    public WebElement getSaveButtonDemoApp() {
+        return (AndroidDriverSetup.getAndroidDriver().findElement(By.xpath(getSaveButton)));
+    }
+
+    public WebElement getEchoedTextDemoApp(String echo) {
+        return (AndroidDriverSetup.getAndroidDriver().findElementByAccessibilityId(echo));
+    }
+
+    public WebElement getOptionDemoApp(String option) {
+        return (AndroidDriverSetup.getAndroidDriver().findElementByAccessibilityId(option));
+    }
+
+    public void scrollToElementWithAccessibilityId(String accessibilityId) {
+        String uiAutomatorString = String.format("new UiScrollable(new UiSelector().scrollable(true)).scrollIntoView(new UiSelector().description(\"%s\"))", accessibilityId);
+        AndroidDriverSetup.getAndroidDriver().findElement(MobileBy.AndroidUIAutomator(uiAutomatorString));
     }
 }
--- src/test/java/step_defination/Android/HomeSteps.java
+++ src/test/java/step_defination/Android/HomeSteps.java
@@ -1,47 +1,52 @@
 package step_defination.Android;
 
 import static com.codeborne.selenide.Selenide.*;
+
 import com.codeborne.selenide.Condition;
+import core.utils.AndroidCore.AndroidDriverSetup;
 import io.cucumber.datatable.DataTable;
 import io.cucumber.java.en.And;
 import io.cucumber.java.en.Then;
 import io.cucumber.java.en.When;
+import org.openqa.selenium.Alert;
+import org.openqa.selenium.NoAlertPresentException;
 import org.testng.Assert;
 import pages.Page;
+
 import java.util.List;
 import java.util.Map;
 
 public class HomeSteps extends Page {
     @When("[Home Page] App should open successfully")
-    public void UserIsOnHomePageTitleTestingApp()  {
+    public void UserIsOnHomePageTitleTestingApp() {
         Assert.assertTrue(getHomePage().getTitle().isDisplayed());
     }
 
-    @And("^\\[Home Page\\] Verify the Version Code (.*)$")
-    public void verifyTheCode(String versionCode) {
-        Assert.assertEquals(getHomePage().getVersionCode().getText(),versionCode,"Verify Version Code");
-    }
-
-    @And("^\\[Home Page\\] Verify the Version Name (.*)$")
-    public void verifyTheName(String versionName) {
-        Assert.assertEquals(getHomePage().getVersionName().getText(),versionName,"Verify Version Name");
-        getHomePage().getVersionName().isDisplayed();
-    }
-
-    @When("\\[Home Page\\] User tap on Button (.*)$")
-    public void homePageUserTapOnButtonXxx(int button) {
-        getHomePage().getButton(button).click();
-    }
-
-    @Then("[Home Page] Verify the Immediate Update button is visible")
-    public void homePageVerifyTheImmediateUpdateButtonIsVisible() {
-        getHomePage().getImmediateButton().isDisplayed();
-    }
-
-    @When("[Home Page] User tap on Immediate Update Button")
-    public void homePageUserTapOnImmediateUpdateButton() {
-        getHomePage().getImmediateButton().click();
-    }
+//    @And("^\\[Home Page\\] Verify the Version Code (.*)$")
+//    public void verifyTheCode(String versionCode) {
+//        Assert.assertEquals(getHomePage().getVersionCode().getText(), versionCode, "Verify Version Code");
+//    }
+//
+//    @And("^\\[Home Page\\] Verify the Version Name (.*)$")
+//    public void verifyTheName(String versionName) {
+//        Assert.assertEquals(getHomePage().getVersionName().getText(), versionName, "Verify Version Name");
+//        getHomePage().getVersionName().isDisplayed();
+//    }
+//
+//    @When("\\[Home Page\\] User tap on Button (.*)$")
+//    public void homePageUserTapOnButtonXxx(int button) {
+//        getHomePage().getButton(button).click();
+//    }
+//
+//    @Then("[Home Page] Verify the Immediate Update button is visible")
+//    public void homePageVerifyTheImmediateUpdateButtonIsVisible() {
+//        getHomePage().getImmediateButton().isDisplayed();
+//    }
+//
+//    @When("[Home Page] User tap on Immediate Update Button")
+//    public void homePageUserTapOnImmediateUpdateButton() {
+//        getHomePage().getImmediateButton().click();
+//    }
 
 //    @Then("[Home Page] Verify the three button options")
 //    public void updatePageVerifyTheThreeButtonOptions(DataTable dataTable) {
@@ -59,19 +64,97 @@ public class HomeSteps extends Page {
 //        }
 //    }
 
-    @Then("\\[Home Page\\] Verify that Result (.*) is displayed$")
-    public void homePageVerifyThatResul1IsVisible(String number) {
-        Assert.assertEquals(getHomePage().getResult().getText(),"RESULT "+number,"Verify Result with Number");
+//    @Then("\\[Home Page\\] Verify that Result (.*) is displayed$")
+//    public void homePageVerifyThatResul1IsVisible(String number) {
+//        Assert.assertEquals(getHomePage().getResult().getText(), "RESULT " + number, "Verify Result with Number");
+//        getHomePage().backButton();
+//    }
+//
+//    @When("[Home Page] User tap on Flexible Update Button")
+//    public void homePageUserTapOnFlexibleUpdateButton() {
+//        getHomePage().getFlexibleButton().click();
+//    }
+//
+//    @Then("[Home Page] Verify the Flexible Update button is visible")
+//    public void homePageVerifyTheFlexibleUpdateButtonIsVisible() {
+//        getHomePage().getFlexibleButton().isDisplayed();
+//    }
+
+    @Then("[Home Page] Verify that The App title is displayed on home page")
+    public void homePageVerifyThatTheAppTitleIsDisplayedOnHomePage() {
+        Assert.assertEquals(getHomePage().getTitleDemoApp().getText(), "The App", "Verify Result");
+    }
+
+    @When("^I tap on (.*) tab$")
+    public void iTapOnXxxTab(String name) {
+        getHomePage().getTabNameDemoApp(name).click();
+    }
+
+    @And("I press back button")
+    public void iPressBackButton() {
         getHomePage().backButton();
     }
 
-    @When("[Home Page] User tap on Flexible Update Button")
-    public void homePageUserTapOnFlexibleUpdateButton() {
-        getHomePage().getFlexibleButton().click();
+    @When("^I type (.*) in text field on echo box screen$")
+    public void iTypeXxxInTextFieldOnEchoBoxScreen(String text) {
+        getHomePage().getTextFieldDemoApp().sendKeys(text);
     }
 
-    @Then("[Home Page] Verify the Flexible Update button is visible")
-    public void homePageVerifyTheFlexibleUpdateButtonIsVisible() {
-        getHomePage().getFlexibleButton().isDisplayed();
+    @When("I tap on save button")
+    public void iTapOnSaveButton() {
+        getHomePage().getSaveButtonDemoApp().click();
+    }
+
+    @Then("^I should see (.*) echoed text on echo box screen$")
+    public void iShouldSeeXxxEchoedTextOnEchoBoxScreen(String echo) {
+        Assert.assertEquals(getHomePage().getEchoedTextDemoApp(echo).getText(), echo, "Verify Result");
+    }
+
+    @When("^I tap on (.*) option$")
+    public void iTapOnXxxOption(String option) {
+        getHomePage().getOptionDemoApp(option).click();
+    }
+
+    @When("^I scroll to (.*) accessibility id$")
+    public void iScrollToXxxAccessibilityId(String id) {
+        getHomePage().scrollToElementWithAccessibilityId(id);
+
+    }
+
+    @And("I accept popup")
+    public void iAcceptPopup() {
+        Alert alert = AndroidDriverSetup.getAndroidDriver().switchTo().alert();
+        alert.accept();
+    }
+
+    @And("I dismiss popup")
+    public void iDismissPopup() {
+        Alert alert = AndroidDriverSetup.getAndroidDriver().switchTo().alert();
+        alert.dismiss();
+    }
+
+    @Then("^I should see popup$")
+    public void iShouldSeePopup() {
+        try {
+            // Attempt to switch to the alert
+            Alert alert = AndroidDriverSetup.getAndroidDriver().switchTo().alert();
+            // If successful, we assume the popup is displayed
+            System.out.println("Popup is displayed with message: " + alert.getText());
+        } catch (NoAlertPresentException e) {
+            // If an exception is caught, it means no popup is present
+            throw new AssertionError("Expected a popup, but none was displayed.");
+        }
+    }
+
+    @And("^I wait (\\d+) seconds$")
+    public void iWaitSeconds(int seconds) {
+        try {
+            // Convert seconds to milliseconds and wait
+            Thread.sleep(seconds * 1000L);
+            System.out.println("Waited " + seconds + " seconds.");
+        } catch (InterruptedException e) {
+            Thread.currentThread().interrupt();
+            System.err.println("Wait was interrupted.");
+        }
     }
 }
